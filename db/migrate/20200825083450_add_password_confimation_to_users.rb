fefe8c0ce6d233650537ebf82d73e70b7f417713
manap1031 <ikme.sabosan00@gmail.com>
2020-08-26 14:07:15 +0900

    不要ファイルの削除

--- db/migrate/20200825083450_add_password_confimation_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordConfimationToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :Password_confimation, :string
-  end
-end
--- db/migrate/20200825085448_add_last_name_kana_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastNameKanaToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :Last_name_kana, :string
-  end
-end
