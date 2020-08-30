a46d77bae460aa0e4bf64411c9d27e5ec5d0e1b1
manap1031 <ikme.sabosan00@gmail.com>
2020-08-30 21:41:34 +0900

    ActiveHash,categoryの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,23 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :condition, :shipping_charge, :shipping_area, :shipping_day
 
   belongs_to :user
+  has_one_attached :image
 
   validates :item_name, presence: true
   validates :describe, presence: true
   validates :category_id, presence: true
   validates :condition_id, presence: true
-  validates :shippong_charge_id, presence: true
+  validates :shipping_charge_id, presence: true
   validates :shipping_area_id, presence: true
   validates :shipping_day_id, presence: true
   validates :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_charge_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
 end
+
