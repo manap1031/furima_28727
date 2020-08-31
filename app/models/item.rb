ac0aaca9cb107ad42095a4bf581b103636d8eb90
manap1031 <ikme.sabosan00@gmail.com>
2020-08-31 15:50:02 +0900

    価格の範囲、半角数字の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :condition, :shipping_charge, :shipping_area, :shipping_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_day
 
   belongs_to :user
   has_one_attached :image
@@ -12,7 +16,10 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, presence: true
   validates :shipping_area_id, presence: true
   validates :shipping_day_id, presence: true
+
   validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to:9999999}
+  
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
