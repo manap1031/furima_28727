db0c6cccc93185121397c44d99d7e4d344045a38
manap1031 <ikme.sabosan00@gmail.com>
2020-09-01 14:46:37 +0900

    販売利益、販売手数料の実装

---
+++ app/javascript/index.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function(){
+
+  let KeyUpPrice = document.getElementById("item-price")
+  let KeyUpTax = document.getElementById("add-tax-price")
+  let KeyUpProfit = document.getElementById("profit")
+
+  KeyUpPrice.addEventListener('keyup', function(){
+   let Price = KeyUpPrice.value;
+   AddTax = Price * 0.1
+   KeyUpTax.innerHTML = AddTax
+   Profit = Price - AddTax
+   KeyUpProfit.innerHTML = Profit
+})
+})
+
