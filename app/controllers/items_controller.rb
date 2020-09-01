9302ab9e8158cf47c9cb78166e68e5fd214d3b98
manap1031 <ikme.sabosan00@gmail.com>
2020-09-01 14:47:41 +0900

    不要な記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,27 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :create]
 
   def index
-    @items = Item.all
+    @items = Item.all.order(id: "DESC")
   end
 
   def new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :describe, :category_id, :condition_id, :shippong_charge_id, :shipping_area_id, :shipping_day_id, :price).marge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :describe, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 
     def move_to_index
