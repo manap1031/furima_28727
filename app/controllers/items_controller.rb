aeb08ce547b3621b3f2ac5af91503432c3686474
manap1031 <ikme.sabosan00@gmail.com>
2020-09-03 16:13:16 +0900

    商品一覧表示機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :find_item]
   before_action :move_to_index, except: [:index, :create, :show, :oder]
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -19,35 +20,40 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
     item = Item.find(params[:id])
     item.update(item_params)
+    if item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
   end
 
+  def order
+    ItemOrder.create(item_id: params[:id])
+    redirect_to root_path
+ end
 
- def order
-  ItemOrder.create(item_id: params[:id])
-  redirect_to root_path
-end
-
-def find_item
-  @item = Item.find(params[:id])
-end
+  def find_item
+  end
 
   private
 
   def item_params
     params.require(:item).permit(:item_name, :describe, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
