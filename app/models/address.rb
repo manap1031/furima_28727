477f8d47a6b9c6e95a275610855acbdb137864a1
manap1031 <ikme.sabosan00@gmail.com>
2020-09-04 17:07:00 +0900

    addressモデルの作成

---
+++ app/models/address.rb
@@ -0,0 +1,11 @@
+class Address < ApplicationRecord
+  belongs_to :purchase
+
+  validates :postal_code, presence: true
+  validates :prefectures_id, presence: true
+  validates :municipalities, presence: true
+  validates :addres_number, presence: true
+  validates :building
+  validates :phone_number, presence: true
+  validates :item_name, presence: true
+end
